f835fac7ad68923e70da27e8876ac504ae8d3380
Daniel Cortes <team@novoeia.com>
2026-07-26 09:01:52 -0400

    ajuste posicion nombre certificado

--- src/pages/CertificadoPage.tsx
+++ src/pages/CertificadoPage.tsx
@@ -67,7 +67,7 @@ export function CertificadoPage() {
         }}/>
         <div ref={nameRef} style={{
           position: 'absolute',
-          top: '53%',
+          top: '55%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
           width: '52%',
